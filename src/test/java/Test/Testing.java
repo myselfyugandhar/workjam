94bedb8c0bdfd321a0b069c9f75e682f6fca7659
Yugandhar Vakkalakula <yvakkalakula@tcs.woolworths.com.au>
2023-09-27 15:55:47 +0530

    k

--- src/test/java/Test/Testing.java
+++ src/test/java/Test/Testing.java
@@ -1,8 +1,19 @@
 package Test;
 
+import org.testng.annotations.Test;
+
 public class Testing {
 
-	public static void main(String args[]) {
+	
+@Test(priority=1)
+public static void workjam1() {
 	System.out.println("This is Sample");
+	
+}
+@Test(priority=2)
+public static void workjam2() {
+	System.out.println("This is Sample2");
+	
 }
 }
+
